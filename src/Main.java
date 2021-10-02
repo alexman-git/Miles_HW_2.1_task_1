3bfa005635966fc960e3bb344083de23b773dc37
Alexander Maletin <man.alex212@gmail.com>
2021-10-02 15:55:04 +0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 10_000;
+        byte oneBonus = 20;
+        int totalBonus = ticketPrice/oneBonus;
+        System.out.println(totalBonus);
+    }
+}
